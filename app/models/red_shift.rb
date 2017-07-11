c678c4912f195f106de68cbdf886195cacd1cf17
Greg Nokes <tsykoduk@gmail.com>
2017-07-11 11:27:53 -0700

    trying with out pool directive

--- app/models/red_shift.rb
+++ app/models/red_shift.rb
@@ -1,15 +1,15 @@
 class RedShift < ApplicationRecord
   self.table_name = "account"
 
-  def initialize             
-    uri = URI.parse(ENV["REDSHIFT"])                                               
-    @conn = PG.connect(                                                                                                        
-     port: uri.port,                                                           
-     pool: 5,                                                                                                        
-     username: uri.user,                                                  
-     password: uri.password,                                         
-     host: uri.host                                               
-    )                                                                       
+  def initialize
+    uri = URI.parse(ENV["REDSHIFT"])
+    @conn = PG.connect(
+     port: uri.port,
+    # pool: 5,
+     username: uri.user,
+     password: uri.password,
+     host: uri.host
+    )
   end 
 
 
