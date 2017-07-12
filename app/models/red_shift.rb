2efb931387c69db0e1d67853441dd1ef43c1a794
Greg Nokes <tsykoduk@gmail.com>
2017-07-12 15:37:22 -0700

    update to redshift gem

--- app/models/red_shift.rb
+++ app/models/red_shift.rb
@@ -2,7 +2,7 @@ class RedShift < ApplicationRecord
   self.table_name = "account"
 
  # def initialize
-   redshift_connection(ENV["REDSHIFT"])
+   RedShift.connection(ENV["REDSHIFT"])
    # uri = URI.parse(ENV["REDSHIFT"])
    # @conn = PG.connect(
    # url = uri
