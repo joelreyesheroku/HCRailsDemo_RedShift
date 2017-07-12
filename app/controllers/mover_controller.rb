60ff820f1bf31ce351d106fe25ce11af5f6bfc60
Greg Nokes <tsykoduk@gmail.com>
2017-07-12 11:49:41 -0700

    adding in a migration for redshift

--- app/controllers/mover_controller.rb
+++ app/controllers/mover_controller.rb
@@ -1,2 +1,15 @@
 class MoverController < ApplicationController
+
+  def counter
+    @Postgres = Account.count
+    @Redshift = RedShift.count
+  end
+  
+  def move_to_rs
+    pg_accounts = Account.all
+    for a in pg_accounts
+      RedShift.create!(a.attributes)
+      end
+    end
+
 end
--- app/models/red_shift.rb
+++ app/models/red_shift.rb
@@ -2,15 +2,16 @@ class RedShift < ApplicationRecord
   self.table_name = "account"
 
  # def initialize
-    uri = URI.parse(ENV["REDSHIFT"])
-    @conn = PG.connect(
-    url = uri
+ establish_connection(ENV["REDSHIFT"])
+   # uri = URI.parse(ENV["REDSHIFT"])
+   # @conn = PG.connect(
+   # url = uri
     # port: uri.port,
     # pool: 5,
     # username: uri.user,
     # password: uri.password,
     # host: uri.host
-    )
+    #)
 #  end 
 
 
