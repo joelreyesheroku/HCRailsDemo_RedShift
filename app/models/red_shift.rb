ad9a3d56fd9fb8213867953abe9754a60d3c82fc
Greg Nokes <tsykoduk@gmail.com>
2017-07-14 10:56:16 -0700

    ok, let's us raw sql to bulk load in sections of 5k rows

--- app/models/red_shift.rb
+++ app/models/red_shift.rb
@@ -12,10 +12,93 @@ class RedShift < ApplicationRecord
     database: "kevcoredshift"
     )
     
-  def self.execute_sql(*sql_array)     
+  def execute_sql(*sql_array)     
     connection.execute(send(:sanitize_sql_array, sql_array))
   end
     
+    
+  def bulk_load_accounts(accounts)
+    puts "starting processing accounts"
+    string_accounts = []
+    for a in accounts
+      temp = ""
+      temp << "("
+      temp <<  RedShift.sanitize(a.billingstreet.to_s)
+      temp << ","
+      temp <<  RedShift.sanitize(a.name.to_s)
+      temp << ","
+      temp <<  RedShift.sanitize(a.lastmodifieddate.to_s)
+      temp << ","
+      temp <<  RedShift.sanitize(a.id.to_s)
+      temp << ","
+      temp <<  RedShift.sanitize(a.website.to_s)
+      temp << ","
+      temp <<  RedShift.sanitize(a.billingcity.to_s)
+      temp << ","
+      temp <<  RedShift.sanitize(a.createddate.to_s)
+      temp << ","
+      temp <<  RedShift.sanitize(a.fax.to_s)
+      temp << ","
+      temp <<  RedShift.sanitize(a.phone.to_s)
+      temp << ","
+      temp <<  RedShift.sanitize(a.billingstate.to_s)
+      temp << ","
+      temp <<  RedShift.sanitize(a.accountsource.to_s)
+      temp << ","
+      temp <<  RedShift.sanitize(a.billingcountry.to_s)
+      temp << ","
+      temp <<  RedShift.sanitize(a.billingpostalcode.to_s)
+      temp << ","
+      temp <<  RedShift.sanitize(a.sfid.to_s)
+      temp << ")"
+      string_accounts << temp
+    end
+    i = 0
+    s_a = ""
+    puts "completeded processing accounts, starting bulk load"
+    for s in string_accounts
+      s_a << s.to_s
+      i = i + 1
+      if i % 5000 == 0
+        sql_statement = "INSERT INTO account (
+          billingstreet,
+          name,
+          lastmodifieddate,
+          id,
+          website,
+          billingcity,
+          createddate, 
+          fax,
+          phone,
+          billingstate,
+          accountsource,
+          billingcountry,
+          billingpostalcode,
+          sfid) VALUES #{s_a}"
+          RedShift.execute_sql(sql_statement)
+        s_a = ""
+      else
+        s_a << ", "
+      end
+    end
+    sql_statement = "INSERT INTO account (
+      billingstreet,
+      name,
+      lastmodifieddate,
+      id,
+      website,
+      billingcity,
+      createddate, 
+      fax,
+      phone,
+      billingstate,
+      accountsource,
+      billingcountry,
+      billingpostalcode,
+      sfid) VALUES #{s_a.chomp(", ")}"
+      RedShift.execute_sql(sql_statement)
+      puts "last transactions committed"
+    end
 
 
  # def initialize
